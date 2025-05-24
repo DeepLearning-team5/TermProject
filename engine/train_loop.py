a717f1548dc6c66da4c735218012e2090ccef2de
Jeonseungwoo1 <swoo1577@gmail.com>
2025-05-24 11:13:38 +0900

    fix: model's paramter & fix evaluator error & Add: Comment on code

--- engine/train_loop.py
+++ engine/train_loop.py
@@ -1,37 +1,64 @@
-
 import torch
 from tqdm import tqdm
 from engine.evaluator import Evaluator
+from torch.optim.lr_scheduler import StepLR
+import os
 
 class Trainer:
+    """
+    Triner class for training and evaluating a mode
+
+    Attributes:
+        model : The object detection model.
+        optimizer: The optimizer for training.
+        train_loader: Dataloader for training dataset.
+        test_loader: Dataloader for evaluation.
+        logger: Logger object (e.g., wandb) to record metrics
+        lr_schedular: Learning rate schedular (step decay)
+        early_stopping: Number of epochs to wait before early stopping if no improvement on performance
+        evaluator: Evaluator object for computing mAP after each epoch.
+    """
     def __init__(self, model, optimizer, train_loader, test_loader, device, logger=None):
+        """
+        Initialize the Trainer.
+        """
         self.model = model
         self.optimizer = optimizer
         self.train_loader = train_loader
         self.test_loader = test_loader
         self.device = device
         self.logger = logger
+        self.lr_schedular = StepLR(optimizer, step_size=7, gamma=0.1)
+        self.early_stopping=10
 
         self.evaluator = Evaluator(self.model, self.test_loader, self.device, self.logger)
 
-    def train(self, epochs, eval_fn = None, save_path=None):
+    def train(self, epochs, save_path=None):
+        """
+        Perform training and evaluation for a given number of epochs.
+        
+        Args:
+            epochs (int): Total number of training epochs.
+            save_path (str): Directory to save checkpoints
+        
+        """
         best_score = -1.0
         early_stopping_counter = 0
 
         for epoch in range(epochs):
             loss = self.train_one_epoch(epoch)
 
-            if self.lr_schedular:
+            if self.lr_schedular is not None:
                 self.lr_schedular.step()
             
             if self.evaluator:
-                
                 score = self.evaluator.evaluate(epoch)
 
                 if score > best_score:
                     best_score = score
                     early_stopping_counter = 0
                     if save_path:
+                        os.makedirs(save_path, exist_ok=True)
                         torch.save(self.model.state_dict(), f"{save_path}/best_model.pth")
                         print(f"[Checkpoint] Best model saved (score: {score:.4f})")
                 else:
@@ -43,14 +70,26 @@ class Trainer:
                     break
             
             if save_path:
+                os.makedirs(save_path, exist_ok=True)
                 torch.save(self.model.state_dict(), f"{save_path}/model_epoch_{epoch}.pth")
             
     def train_one_epoch(self, epoch):
+        """
+        Train the model for one epoch.
+        
+        Args:
+            epoch (int): Current epoch index.
+            
+        Returns:
+            avg_loss (float): Average loss for the epoch.
+        """
         self.model.train()
         total_loss = 0.0
 
         pbar = tqdm(self.train_loader, desc=f"[Train] Epoch {epoch}")
-        for step, (images, targets) in enumerate(pbar):
+        for images, targets in pbar:
+            if len(images) == 0:
+                continue
             images = [img.to(self.device) for img in images]
             targets = [{k: v.to(self.device) for k, v in t.items()} for t in targets]
 
@@ -71,16 +110,15 @@ class Trainer:
                     "train/loss_box_reg": loss_dict["loss_box_reg"].item(),
                     "train/loss_objectness": loss_dict["loss_objectness"].item(),
                     "train/loss_rpn_box_reg": loss_dict["loss_rpn_box_reg"].item(),
-                    "step": step + epoch * len(self.train_loader)
                 })
 
-            avg_loss = total_loss / len(self.train_loader)
-            print(f"[Train] Avg Loss: {avg_loss:.4f}")
+        avg_loss = total_loss / len(self.train_loader)
+        print(f"[Train] Avg Loss: {avg_loss:.4f}")
 
-            if self.logger:
-                self.logger.log({
-                    "train/loss_epoch": avg_loss,
-                    "epoch": epoch
-                })
+        if self.logger:
+            self.logger.log({
+                "train/loss_epoch": avg_loss,
+                "epoch": epoch
+            })
 
-            return avg_loss
+        return avg_loss
--- model.py
+++ model.py
@@ -4,18 +4,40 @@ from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 
 
 def build_faster_rcnn(config):
-    weights = FasterRCNN_ResNet50_FPN_Weights.DEFAULT if config.pretrained else None
-    model = fasterrcnn_resnet50_fpn(min_size=64, max_size=224, weights=weights)
+    """
+    Build a Faster R-CNN model based on ResNet-50 FPN backbone
 
+    Args:
+        config: Congfiguration object with the following attributes:
+
+            - pretrained (bool): Whether to load COCO pretrained weights.
+            - backbone_freeze (bool): Whether to freeze the backbone layers.
+            - head_random_init (bool): Whether to randomly initialize the detection head. (for trian head on scratch)
+            - num_classes (int): Number of object classes (excluding background).
+            - image_size (tuple or int): Image size for min_size/max_size if needed.
+
+    Return:
+        model: A customized Faster R-CNN model.
+    """
+
+    # Load pretrained weights 
+    weights = FasterRCNN_ResNet50_FPN_Weights.DEFAULT if config.pretrained else None
+    model = fasterrcnn_resnet50_fpn(
+        min_size=320, 
+        max_size=320, 
+        weights=weights
+    )
+
+    # Freeze Backbone
     if config.backbone_freeze:
         for param in model.backbone.parameters():
             param.requires_grad = False
 
-    # 3. Replace head
+    # Replace head
     in_features = model.roi_heads.box_predictor.cls_score.in_features
     model.roi_heads.box_predictor = FastRCNNPredictor(in_features, config.num_classes)
 
-    # 4. (Optional) Random initialize head — for semi-scratch
+    # Random initialize head — for semi-scratch
     if config.head_random_init:
         torch.nn.init.normal_(model.roi_heads.box_predictor.cls_score.weight, std=0.01)
         torch.nn.init.constant_(model.roi_heads.box_predictor.cls_score.bias, 0)
