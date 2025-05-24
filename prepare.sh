be8910ce36826a302e507251837c1cdf2d47a483
Jeonseungwoo1 <swoo1577@gmail.com>
2025-05-24 12:14:06 +0900

    fix: prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,12 +1,16 @@
+mkdir -p .data
+cd .data
+
 gdown 1LvxwCOfUa-OklIvBJhB8zJlochjJiPFS
 gdown 1fa2L6oaPSjZ1_WqlTmIp6i2RbdR2y1Pw
-gdown 1bZtVWcxxFrijE_ALvNPjH1MXIKio6BIr
+down 1bZtVWcxxFrijE_ALvNPjH1MXIKio6BIr
 
-names=(clipart watercolor comic)
+names=(clipart watercolor comic) 
 for name in "${names[@]}"
 do
     unzip ${name}.zip
     rm ${name}.zip
 done
 
-python utils/converToCOCO.py
+cd ..
+python utils/convertToCOCO.py
